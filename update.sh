e7bf3cbd6c8ca478028356fb1fe3c0a6368b398e
Arthur Chang <arthurc0102@gmail.com>
2018-12-31 16:10:58 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-rm -r build/ dist django_jsoneditor_widget.egg-info && python setup.py sdist bdist_wheel && twine upload dist/*
+rm -rf build dist django_jsoneditor_widget.egg-info && python setup.py sdist bdist_wheel && twine upload dist/*
